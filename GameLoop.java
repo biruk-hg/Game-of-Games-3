042da9003fc3359dc99675c6d807384de50a6cff
kwaku.aa <kwakuagyapong478@gmail.com>
2024-12-05 16:13:37 -0500

    my ide broke, saving thee files. Finishing the implementation of main loop. UNTESTED

--- GameLoop.java
+++ GameLoop.java
@@ -1,11 +1,29 @@
-public class GameLoop {
+public class GameLoop extends GameOfGames{
     public static void main(String args[]) {
         /* determine if winner has been chosen to close the game/end loop */
         Boolean WINNER_CHOSEN = false;
-        while (WINNER_CHOSEN != true) {
+        Boolean GAME_QUIT = false;
+        
+        System.out.println("Welcome to the Game of Games!");
+        
+        /* enter main game loop */
+        while (WINNER_CHOSEN && GAME_QUIT != true) {
+            GameOfGames maingame = new GameOfGames();
+
+            /* potentially skip the player initialization stage if in debug mode */
+            Player P1 = new Player();
+            Player P2 = new Player();
             
+            //*******************************************************************************
+            // NOT FINAL CODE. TESTING CURRENT METHODS/CLASSES TO SIMULATE THE FINAL GAMELOOP
+            //*******************************************************************************
 
+            /* current player selects a game */
+            maingame.game_select();
+            /* plays game1, player 1 wins */
+            P1.increase_score();
+            maingame.show_scoreboard();
+            maingame.game_quit();
         }
-
     }
 }
--- GameOfGames.java
+++ GameOfGames.java
@@ -1,27 +1,63 @@
+import java.util.Scanner;
+
 public class GameOfGames{
     /* detect gamemode for developer purposes */
-    Boolean DEBUG = false;
-    /* keep track of player scores after games */
-    int PLAYER_1_SCORE, PLAYER_2_SCORE = 0;
+    public Boolean DEBUG = false;
+    /* scanner(sc) to keep track of inputs */
+    Scanner sc;
+    /* selected game */
+    public int SELECTED_GAME;
+    /* keeping track of players */
+    Player TESTER;
+    Player P1;
+    Player P2;
 
-
-    /* normal gameplay mode */
+    /* start a game of games */
     public GameOfGames() {
-    }
-    /* testing/debug mode, enable to get verbose output */
-    public GameOfGames(Boolean DEBUG) {
+        sc = new Scanner(System.in);
     }
 
     /* display main menu + start game and select game options */
-    void main_menu() {
-
+    void game_select() {
+        int selected_game;
+        System.out.println("Please enter a game to select and play: \n1-Coin Flip \n2-Find the Thimble \n3-Guess the Number \n4-Even and Odd \n5-Find the Red Thread \n\nChoose from 1-5: ");
+        selected_game = sc.nextInt();
+        if (selected_game == 0) {
+            DEBUG = true;
+            System.out.println("Debug mode enabled\n");
+            game_select();
+        }
+        else if (selected_game == 1 || selected_game == 2 || selected_game == 3 || selected_game == 4 || selected_game == 5 ){
+            SELECTED_GAME = selected_game;
+        }
+        else {
+            System.out.println("Invalid input detected. Please try again \n");
+            game_select();
+        }
     }
 
-    /* collect/update scoreboard information */
-    String update_scoreboard() {
-        return "hi";
+    /* allow players to quit game if desired */
+    boolean game_quit() {
+        String game_quit;
+        System.out.println("Quit game? y/n\n");
+        game_quit = sc.nextLine();
+        if (game_quit == "y") {
+            return true;
+        }
+        else if (game_quit == "n") {
+            return false;
+        }
+        else {
+           return false;
+        }
     }
 
-    /*  */
-
+    /* display scoreboard information to players */
+    void show_scoreboard() {
+        System.out.println("******** SCOREBOARD ********\n");
+        System.out.println("*****PLAYER*******SCORE*****\n");
+        System.out.printf("\t%s\t%d", P1.get_name(), P1.get_score());
+        System.out.printf("\t%s\t%d", P2.get_name(), P2.get_score());
+    
+    }
 }
