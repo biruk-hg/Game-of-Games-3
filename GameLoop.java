54e038f5a73b9326740011c932a77cfcd4526a14
kwaku.aa <kwakuagyapong478@gmail.com>
2024-12-05 02:25:01 -0500

    initial class setups, couple method declarations

---
+++ GameLoop.java
@@ -0,0 +1,11 @@
+public class GameLoop {
+    public static void main(String args[]) {
+        /* determine if winner has been chosen to close the game/end loop */
+        Boolean WINNER_CHOSEN = false;
+        while (WINNER_CHOSEN != true) {
+            
+
+        }
+
+    }
+}
---
+++ GameOfGames.java
@@ -0,0 +1,27 @@
+public class GameOfGames{
+    /* detect gamemode for developer purposes */
+    Boolean DEBUG = false;
+    /* keep track of player scores after games */
+    int PLAYER_1_SCORE, PLAYER_2_SCORE = 0;
+
+
+    /* normal gameplay mode */
+    public GameOfGames() {
+    }
+    /* testing/debug mode, enable to get verbose output */
+    public GameOfGames(Boolean DEBUG) {
+    }
+
+    /* display main menu + start game and select game options */
+    void main_menu() {
+
+    }
+
+    /* collect/update scoreboard information */
+    String update_scoreboard() {
+        return "hi";
+    }
+
+    /*  */
+
+}
