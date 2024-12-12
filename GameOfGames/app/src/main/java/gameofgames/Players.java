17c1485cd125e4319087a56a385eabe2252cb0c5
Biruktayit <156105544+biruk-hg@users.noreply.github.com>
2024-12-12 09:39:15 -0500

    Create Players.java

---
+++ GameOfGames/app/src/main/java/gameofgames/Players.java
@@ -0,0 +1,26 @@
+// Player Class
+public class Players {
+    private String name;
+    private int score;
+
+    public Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void incrementScore() {
+        score++;
+    }
+
+    public void resetScore() {
+        score = 0;
+    }
+}
