12a174b577bda9921fb518d31b1d29e9204866ab
Biruktayit <156105544+biruk-hg@users.noreply.github.com>
2024-12-16 09:28:47 -0500

    Update EvenOrOdd.java

--- GameOfGames/app/src/main/EvenOrOdd.java
+++ GameOfGames/app/src/main/EvenOrOdd.java
@@ -5,45 +5,61 @@ public class EvenOrOdd {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Even or Odd, " + player.getName() + "!");
 
-        System.out.print("Enter the number of rounds to play (odd number): ");
-        int rounds = scanner.nextInt();
-
-        while (rounds % 2 == 0 || rounds <= 0) {
-            System.out.print("Enter a positive odd number for rounds: ");
-            rounds = scanner.nextInt();
-        }
-
+        int rounds = getBestOutOf(scanner);
         int playerWins = 0, computerWins = 0;
 
-        for (int i = 1; i <= rounds; i++) {
-            System.out.println();
-            System.out.println("Round " + i + ": Guess if the number is even or odd.");
-            String playerGuess = scanner.next();
+        System.out.print("Do you want to be 'even' or 'odd'? ");
+        String playerChoice = scanner.next().toLowerCase();
+        while (!playerChoice.equals("even") && !playerChoice.equals("odd")) {
+            System.out.print("Invalid input. Please enter 'even' or 'odd': ");
+            playerChoice = scanner.next().toLowerCase();
+        }
 
-            int randomNumber = (int) (Math.random() * 100);
-            System.out.println("Generated number: " + randomNumber);
-            String actualResult = randomNumber % 2 == 0 ? "even" : "odd";
+        String computerChoice = playerChoice.equals("even") ? "odd" : "even";
+        System.out.println("You chose: " + playerChoice + " | Computer is: " + computerChoice);
 
-            if (playerGuess.equalsIgnoreCase(actualResult)) {
-                System.out.println("You guessed correctly!");
+        while (playerWins < rounds / 2 + 1 && computerWins < rounds / 2 + 1) {
+            System.out.print("Enter a number between 1 and 5: ");
+            int playerNumber = scanner.nextInt();
+            while (playerNumber < 1 || playerNumber > 5) {
+                System.out.print("Invalid number. Please enter a number between 1 and 5: ");
+                playerNumber = scanner.nextInt();
+            }
+
+            int computerNumber = (int) (Math.random() * 5) + 1;
+            System.out.println("Computer chose: " + computerNumber);
+
+            int sum = playerNumber + computerNumber;
+            System.out.println("Sum is: " + sum);
+
+            if ((sum % 2 == 0 && playerChoice.equals("even")) || (sum % 2 != 0 && playerChoice.equals("odd"))) {
+                System.out.println("You win this round!");
                 playerWins++;
             } else {
                 System.out.println("Computer wins this round!");
                 computerWins++;
             }
 
-            if (playerWins > rounds / 2 || computerWins > rounds / 2) {
-                break;
-            }
+            System.out.println("Score: You - " + playerWins + " | Computer - " + computerWins);
         }
 
         if (playerWins > computerWins) {
             System.out.println("Congratulations, " + player.getName() + "! You won the game.");
             player.incrementScore();
-            System.out.println();
         } else {
             System.out.println("Computer wins! Better luck next time.");
-            System.out.println();
         }
+    }
+
+    private int getBestOutOf(Scanner scanner) {
+        System.out.print("Enter an odd number for the Best out of value: ");
+        int rounds = scanner.nextInt();
+
+        while (rounds % 2 == 0 || rounds <= 0) {
+            System.out.print("Invalid input. Please enter a positive odd number: ");
+            rounds = scanner.nextInt();
+        }
+
+        return rounds;
     }
 }
