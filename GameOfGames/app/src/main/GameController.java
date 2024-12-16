6a18fcac952e777e5c3b632b924199ef199362eb
Biruktayit <156105544+biruk-hg@users.noreply.github.com>
2024-12-16 09:49:31 -0500

    Update GameController.java

--- GameOfGames/app/src/main/GameController.java
+++ GameOfGames/app/src/main/GameController.java
@@ -3,34 +3,61 @@ import java.util.Scanner;
 public class GameController {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Game of Games!");
         System.out.print("Enter your name: ");
-        Players player = new Players(scanner.nextLine());
+        String playerName = scanner.nextLine();
+        Players player = new Players(playerName);
+
+        int computerScore = 0;
 
         while (true) {
-            System.out.println("\nChoose a game to play (1-6):");
+            System.out.println("\nChoose a game to play:");
             System.out.println("1. Coin Flip");
-            System.out.println("2. Even or Odd");
-            System.out.println("3. Find the Thimble");
+            System.out.println("2. Find the Thimble");
+            System.out.println("3. Even or Odd");
             System.out.println("4. Guess the Number");
             System.out.println("5. Find the Red Thread");
             System.out.println("6. Exit");
+            System.out.print("Enter your choice: ");
 
             int choice = scanner.nextInt();
-            System.out.println();
             switch (choice) {
-                case 1: new CoinFlip().startGame(player); break;
-                case 2: new EvenOrOdd().startGame(player); break;
-                case 3: new FindTheThimble().startGame(player); break;
-                case 4: new GuessTheNumber().startGame(player); break;
-                case 5: new RedThread().startGame(player); break;
-                case 6: {
-                    System.out.println("Thanks for playing, " + player.getName() + "! Your final score is: " + player.getScore());
+                case 1:
+                    CoinFlip coinFlipGame = new CoinFlip();
+                    coinFlipGame.startGame(player);
+                    computerScore += coinFlipGame.getComputerScore();
+                    break;
+                case 2:
+                    FindTheThimble findTheThimbleGame = new FindTheThimble();
+                    findTheThimbleGame.startGame(player);
+                    computerScore += findTheThimbleGame.getComputerScore();
+                    break;
+                case 3:
+                    EvenOrOdd evenOrOddGame = new EvenOrOdd();
+                    evenOrOddGame.startGame(player);
+                    computerScore += evenOrOddGame.getComputerScore();
+                    break;
+                case 4:
+                    GuessTheNumber guessTheNumberGame = new GuessTheNumber();
+                    guessTheNumberGame.startGame(player);
+                    computerScore += guessTheNumberGame.getComputerScore();
+                    break;
+                case 5:
+                    RedThread redThreadGame = new RedThread();
+                    redThreadGame.startGame(player);
+                    computerScore += redThreadGame.getComputerScore();
+                    break;
+                case 6:
+                    System.out.println("Thank you for playing, " + player.getName() + "!");
+                    System.out.println("Final Scoreboard:");
+                    System.out.println(player.getName() + "'s Score: " + player.getScore());
+                    System.out.println("Computer's Score: " + computerScore);
                     scanner.close();
                     return;
-                }
-                default: System.out.println("Invalid choice. Please try again.");
+                default:
+                    System.out.println("Invalid choice. Please try again.");
             }
         }
     }
 }
-
